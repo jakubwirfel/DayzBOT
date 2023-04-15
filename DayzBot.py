b289af994cc8fc825c948ede40104be026e6b741
jakubwirfel <jakubwirfel@gmail.com>
2023-04-15 08:25:32 +0200

    Add menu for functionalities

--- DayzBot.py
+++ DayzBot.py
@@ -1,6 +1,44 @@
 from helpers.threads_helper import ThreadHelper
 from utils.file_utils import FileUtils
 
-if __name__ == "__main__":
+menu_options = {
+    1: 'Anti Ride',
+    2: 'Riding 10min',
+    3: 'Option 3',
+}
+
+
+def print_menu():
+    for key in menu_options.keys():
+        print(key, '--', menu_options[key])
+
+
+def option1():
     FileUtils()
     ThreadHelper().player_executor()
+
+
+def option2():
+    print('Handle option \'Option 2\'')
+
+
+def option3():
+    print('Handle option \'Option 3\'')
+
+
+if __name__ == "__main__":
+    while True:
+        print_menu()
+        option = ''
+        try:
+            option = int(input('Enter your choice: '))
+        except KeyboardInterrupt:
+            exit()
+        if option == 1:
+            option1()
+        elif option == 2:
+            option2()
+        elif option == 3:
+            option3()
+        else:
+            print('Invalid option. Please enter a number between 1 and 3.')
